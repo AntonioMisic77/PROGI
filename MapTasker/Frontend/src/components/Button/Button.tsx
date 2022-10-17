80f717c8537221571252112846718f4195565e35
bpuvaca <62650091+bpuvaca@users.noreply.github.com>
2022-10-17 12:20:26 +0200

    Clean up code and create first custom component

---
+++ MapTasker/Frontend/src/components/Button/Button.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+
+interface ButtonProps {
+   onClick: () => void;
+   text: string;
+}
+
+const Button = (props: ButtonProps) => {
+   return (  
+      <button onClick={props.onClick}>
+         {props.text}
+      </button>   
+   );
+}
+ 
+export default Button;
