02bf4e4685f4bdf2dc38a36e747e4b2081bf3709
Antonio <antoniohej@gmail.com>
2022-11-06 20:37:24 +0100

    Fix: Navigation Properties in Dto

--- MapTasker/Backend/Data/User/UserDto.cs
+++ MapTasker/Backend/Data/User/UserDto.cs
@@ -31,5 +31,11 @@ namespace Backend.Data.Register
         [Required]
         public int RoleId { get; set; }
 
+
+        // Navigation Properties
+        public virtual Role Role { get; set; } = null!;
+        public virtual ICollection<Comment> Comments { get; set; }
+        public virtual ICollection<Operation> Operations { get; set; }
+
     }
 }
