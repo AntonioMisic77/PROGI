a4a608e4a06b3507486da7bec51511d3f0a2f633
bpuvaca <62650091+bpuvaca@users.noreply.github.com>
2022-11-12 14:27:20 +0100

    Add login page

--- MapTasker/Frontend/src/components/App/App.tsx
+++ MapTasker/Frontend/src/components/App/App.tsx
@@ -4,13 +4,15 @@ import {
    Route,
    Routes,
  } from "react-router-dom";
-
+ 
+import LoginPage from '../../pages/Login/LoginPage';
 import RegisterPage from '../../pages/Registration/RegisterPage';
 
 function App() {
    return (
       <Router>
          <Routes>
+            <Route path="/login" element={<LoginPage/>}/>
             <Route path="/register" element={<RegisterPage/>}/>
             <Route path="/" element={<h1>Za Emin homepage</h1>}/>
          </Routes>
