6ae8528761eb0984a786a70d98b8dcc3c0fbc086
Antonio <antoniohej@gmail.com>
2022-11-14 15:43:11 +0100

    'newCommit'

--- MapTasker/Frontend/src/Api/Api.ts
+++ MapTasker/Frontend/src/Api/Api.ts
@@ -8,6 +8,102 @@
 /* eslint-disable */
 // ReSharper disable InconsistentNaming
 
+export class LoginClient {
+    private http: { fetch(url: RequestInfo, init?: RequestInit): Promise<Response> };
+    private baseUrl: string;
+    protected jsonParseReviver: ((key: string, value: any) => any) | undefined = undefined;
+
+    constructor(baseUrl?: string, http?: { fetch(url: RequestInfo, init?: RequestInit): Promise<Response> }) {
+        this.http = http ? http : window as any;
+        this.baseUrl = baseUrl !== undefined && baseUrl !== null ? baseUrl : "";
+    }
+
+    login(user: LoginDto): Promise<string> {
+        let url_ = this.baseUrl + "/api/Login";
+        url_ = url_.replace(/[?&]$/, "");
+
+        const content_ = JSON.stringify(user);
+
+        let options_: RequestInit = {
+            body: content_,
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            }
+        };
+
+        return this.http.fetch(url_, options_).then((_response: Response) => {
+            return this.processLogin(_response);
+        });
+    }
+
+    protected processLogin(response: Response): Promise<string> {
+        const status = response.status;
+        let _headers: any = {}; if (response.headers && response.headers.forEach) { response.headers.forEach((v: any, k: any) => _headers[k] = v); };
+        if (status === 200) {
+            return response.text().then((_responseText) => {
+            let result200: any = null;
+            result200 = _responseText === "" ? null : JSON.parse(_responseText, this.jsonParseReviver) as string;
+            return result200;
+            });
+        } else if (status !== 200 && status !== 204) {
+            return response.text().then((_responseText) => {
+            return throwException("An unexpected server error occurred.", status, _responseText, _headers);
+            });
+        }
+        return Promise.resolve<string>(null as any);
+    }
+}
+
+export class RegisterClient {
+    private http: { fetch(url: RequestInfo, init?: RequestInit): Promise<Response> };
+    private baseUrl: string;
+    protected jsonParseReviver: ((key: string, value: any) => any) | undefined = undefined;
+
+    constructor(baseUrl?: string, http?: { fetch(url: RequestInfo, init?: RequestInit): Promise<Response> }) {
+        this.http = http ? http : window as any;
+        this.baseUrl = baseUrl !== undefined && baseUrl !== null ? baseUrl : "";
+    }
+
+    register(user: UserDto): Promise<UserDto> {
+        let url_ = this.baseUrl + "/api/Register";
+        url_ = url_.replace(/[?&]$/, "");
+
+        const content_ = JSON.stringify(user);
+
+        let options_: RequestInit = {
+            body: content_,
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            }
+        };
+
+        return this.http.fetch(url_, options_).then((_response: Response) => {
+            return this.processRegister(_response);
+        });
+    }
+
+    protected processRegister(response: Response): Promise<UserDto> {
+        const status = response.status;
+        let _headers: any = {}; if (response.headers && response.headers.forEach) { response.headers.forEach((v: any, k: any) => _headers[k] = v); };
+        if (status === 200) {
+            return response.text().then((_responseText) => {
+            let result200: any = null;
+            result200 = _responseText === "" ? null : JSON.parse(_responseText, this.jsonParseReviver) as UserDto;
+            return result200;
+            });
+        } else if (status !== 200 && status !== 204) {
+            return response.text().then((_responseText) => {
+            return throwException("An unexpected server error occurred.", status, _responseText, _headers);
+            });
+        }
+        return Promise.resolve<UserDto>(null as any);
+    }
+}
+
 export class UserClient {
     private http: { fetch(url: RequestInfo, init?: RequestInit): Promise<Response> };
     private baseUrl: string;
@@ -19,7 +115,7 @@ export class UserClient {
     }
 
     getAllUsers(): Promise<User[]> {
-        let url_ = this.baseUrl + "/User";
+        let url_ = this.baseUrl + "/api/User";
         url_ = url_.replace(/[?&]$/, "");
 
         let options_: RequestInit = {
@@ -52,6 +148,23 @@ export class UserClient {
     }
 }
 
+export interface LoginDto {
+    email: string;
+    password: string;
+}
+
+export interface UserDto {
+    userName?: string | undefined;
+    oib: number;
+    photo: string;
+    password: string;
+    firstName: string;
+    lastName: string;
+    phoneNumber: string;
+    email: string;
+    roleId: number;
+}
+
 export interface User {
     username?: string | undefined;
     oib: number;
@@ -64,6 +177,7 @@ export interface User {
     roleId: number;
     confirmed: boolean;
     role: Role;
+    comments: Comment[];
     operations: Operation[];
 }
 
@@ -71,6 +185,25 @@ export interface Role {
     id: number;
     name: string;
     users: User[];
+}
+
+export interface Comment {
+    id: number;
+    reportId: number;
+    text: string;
+    userOib?: number | undefined;
+    report: MissingReport;
+    userOibNavigation?: User | undefined;
+}
+
+export interface MissingReport {
+    id: number;
+    firstName: string;
+    lastName: string;
+    oib: number;
+    photo: string;
+    description?: string | undefined;
+    comments: Comment[];
 }
 
 export interface Operation {
