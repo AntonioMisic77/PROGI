16c942952724a902105704ecd4cdec6553820b30
JureUglesic <jurica.uglesic@fer.hr>
2022-11-15 17:58:28 +0100

    dodani use caseovi 4 do 8

---
+++ MapTasker/Backend/Controllers/CommentController.cs
@@ -0,0 +1,34 @@
+﻿using Backend.Data.Comment;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Backend.Services.Comment;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommentController : ControllerBase
+    {
+        private readonly IComment _commentService;
+
+        public CommentController(IComment commentService)
+        {
+            _commentService = commentService;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<CommentDto>> CreateComment()
+        {
+            return await _commentService.CreateComment();
+        }
+
+        [HttpDelete]
+
+        public async Task<ActionResult<CommentDto>> DeleteComment(int id)
+        {
+            return await _commentService.DeleteComment(id);
+        }
+    }
+
+
+}
