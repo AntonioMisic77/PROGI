342d79c3dcfad3d85a1ceadecf7eb844cbab72b2
Antonio <antoniohej@gmail.com>
2022-11-15 19:48:40 +0100

    Building outline added.

---
+++ MapTasker/Backend/Services/Building/Building.cs
@@ -0,0 +1,17 @@
+﻿using Backend.Data.Building;
+
+namespace Backend.Services.Building
+{
+    public class Building : IBuilding
+    {
+        public Task<BuildingDto> CreateBuilding(BuildingDto building)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<BuildingDto> UpdateBuildingStatus(BuildingDto building)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MapTasker/Backend/Services/Building/IBuilding.cs
@@ -0,0 +1,11 @@
+﻿using Backend.Data.Building;
+
+namespace Backend.Services.Building
+{
+    public interface IBuilding
+    {
+        Task<BuildingDto> CreateBuilding(BuildingDto building);
+
+        Task<BuildingDto> UpdateBuildingStatus(BuildingDto building);
+    }
+}
