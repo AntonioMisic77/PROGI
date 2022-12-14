6cb9313d4b00a07f83540434b51ede7d0e620723
mj52951 <mj52951@fer.hr>
2022-12-14 15:55:26 +0100

    Implementiran operation

--- MapTasker/Backend/Services/Operation/Operation.cs
+++ MapTasker/Backend/Services/Operation/Operation.cs
@@ -1,18 +1,70 @@
 ﻿
 using Backend.Data.OperationDTO;
+using Backend.Models;
 
 namespace Backend.Services.Operation
 {
     public class Operation : IOperation
     {
-        public Task<OperationDto> CreateOperation(OperationDto operation)
+        private readonly IMapper _mapper;
+        private readonly MapTaskerDBContext _context;
+
+        public Operation(IMapper mapper, MapTaskerDBContext context)
         {
-            throw new NotImplementedException();
+            _mapper = mapper;
+            _context = context;
         }
 
-        public Task<OperationDto> UpdateOperationStatus(OperationDto operation)
+
+        public async Task<OperationDto> CreateOperation(OperationDto dto)
         {
-            throw new NotImplementedException();
+            var user = _context.Users.Find(dto.LeaderOib); 
+            if (user == null)
+            {
+                throw new InvalidDataException("Ne postoji osoba s navedenim OIB-om"); 
+            }
+
+            var role = _context.Roles.Find(user.RoleId);
+            if (role.Name != "Leader" || role.Name != "Voditelj") 
+            {
+                throw new Exception("Osoba nije voditelj grupe"); 
+            }
+
+            var operation = new Backend.Models.Operation() 
+            { 
+                Status = dto.Status,
+                LeaderOib = dto.LeaderOib,
+            };
+            
+            foreach(var region in dto.Regions)
+            {
+                operation.Regions.Add(region); 
+            }
+
+            await _context.AddAsync(operation);
+            await _context.SaveChangesAsync();
+
+            return _mapper.Map<OperationDto>(operation);
+        }
+
+        public async Task<OperationDto> UpdateOperation(OperationDto dto)
+        {
+            var operation = await _context.FirstOrDefaultAsync(a => a.id == dto.id); 
+
+            if (operation == null)
+            {
+                throw new Exception("Ne postoji operacija s tim id-om.");
+            }
+
+            operation.Status = dto.Status;
+            operation.LeaderOib = dto.LeaderOib;
+            operation.Regions = dto.Regions;
+            
+            _context.Attach(operation); 
+            _context.Entry(operation).State = EntityState.Modified; 
+            await _context.SaveChangesAsync();
+
+            return _mapper.Map<OperationDto>(operation); 
         }
     }
 }
