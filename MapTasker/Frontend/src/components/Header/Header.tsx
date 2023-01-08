05530a037ea94161a1d01fd77598805a3503d956
ev2602 <ev53116@fer.hr>
2023-01-08 16:43:26 +0100

    Added edit profile and sign out on header

--- MapTasker/Frontend/src/components/Header/Header.tsx
+++ MapTasker/Frontend/src/components/Header/Header.tsx
@@ -1,8 +1,16 @@
+import styled from "@emotion/styled";
 import { AppBar, Toolbar, Typography, Button } from "@mui/material"
+import { useState } from "react";
 import { Link as RouterLink } from "react-router-dom";
+import { useUserData } from "../../hooks/useUserData";
 
+import "./Header.css"
 
- const headersData = [
+ const headersDataNotLoggedIn = [
+    {
+        label: "Nestale osobe",
+        href: "/missing-reports"
+    },
     {
         label: "Prijava", 
         href: "/login",
@@ -10,10 +18,36 @@ import { Link as RouterLink } from "react-router-dom";
     {
         label: "Registracija", 
         href: "/register",
-    },
+    }, 
+    
  ];
 
+ const headersDataLoggedIn = [
+    {
+        label: "Nestale osobe",
+        href: "/missing-reports"
+    },
+    {
+        label: "Uredi profil",
+        href: "/profile", 
+        className: "not-visible",
+        
+    },
+    {
+        label: "Odjava", 
+        href: ""
+    },
+    
+ ];
+
+
 export default function Header() {
+
+    const [show, setShow] = useState(true);
+
+    let {user, userLoaded} = useUserData();
+
+    console.log(userLoaded)
 
     const displayDesktop = () => {
         return (
@@ -38,30 +72,61 @@ export default function Header() {
     );
 
     const getMenuButtons = () => {
-        return headersData.map(({label, href}) => {
-            return (
-                <Button 
-                {...{
-                    key: label,
-                    color: "inherit", 
-                    to: href, 
-                    component: RouterLink, 
-                }}
-                sx = {{
-                    fontFamily: "Open Sans, sans-serif",
-                    fontWeight: 500,
-                    size: "18px",
-                    marginRight: "2px",
-                    '&:hover': {
-                       backgroundColor: "#9500ae",
-                       color: "#fff",
-                    },
-                }}>
-                    {label}
-                </Button>
+        if(!userLoaded){
+            return headersDataNotLoggedIn.map(({label, href}) => {
                 
-            )
-        })
+                    return (
+                        <Button 
+                        {...{
+                            key: label,
+                            color: "inherit", 
+                            to: href, 
+                            component: RouterLink, 
+                        }}
+                        sx = {{
+                            fontFamily: "Open Sans, sans-serif",
+                            fontWeight: 500,
+                            size: "18px",
+                            marginRight: "2px",
+                            '&:hover': {
+                            backgroundColor: "#9500ae",
+                            color: "#fff",
+                            },
+                        }}
+                        >
+                            {label}
+                        </Button>
+                        
+                    )
+            })
+        } else {
+            return headersDataLoggedIn.map(({label, href}) => {
+                
+                return (
+                    <Button 
+                    {...{
+                        key: label,
+                        color: "inherit", 
+                        to: href, 
+                        component: RouterLink, 
+                    }}
+                    sx = {{
+                        fontFamily: "Open Sans, sans-serif",
+                        fontWeight: 500,
+                        size: "18px",
+                        marginRight: "2px",
+                        '&:hover': {
+                        backgroundColor: "#9500ae",
+                        color: "#fff",
+                        },
+                    }}
+                    >
+                        {label}
+                    </Button>
+                    
+                )
+            })
+        }
     }
 
 
