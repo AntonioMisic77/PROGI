72fe13766abfa13e1652c894e867604b2ec841e8
ev2602 <ev53116@fer.hr>
2023-01-10 15:12:52 +0100

    Added button for missing person

--- MapTasker/Frontend/src/components/MissingReports/MissingReports.tsx
+++ MapTasker/Frontend/src/components/MissingReports/MissingReports.tsx
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from 'react';
 import { useUserData } from '../../hooks/useUserData';
 import { useNavigate } from 'react-router-dom';
 import { MissingReportClient, MissingReportDto } from '../../Api/Api';
-import { Card, CardContent, Typography } from '@mui/material';
+import { Button, Card, CardContent, Typography } from '@mui/material';
 import MissingReportCard from '../MissingReportCard/MissingReportCard';
 import { date } from 'yup/lib/locale';
 
@@ -37,6 +37,30 @@ const MissingReports = () =>  {
             }}>
                 Nestale osobe
             </Typography>
+            <div style={{
+                display: 'flex',
+                justifyContent: 'center',
+                padding: '20px'
+            }}>
+                <Typography sx={{
+                    textAlign: "center",
+                    fontFamily: 'Work Sans, sans-serif', 
+                    color: '#616161', 
+                    fontSize: "22px"
+                }}>
+                    Želite li prijaviti nestalu osobu?
+                </Typography>
+                <Button 
+                    variant="contained"
+                    size="medium"
+                    sx={{
+                        marginLeft: '20px'
+                    }}
+                    href="/missing-person"
+                >
+                    PRIJAVI
+                </Button>
+            </div>
             <div className="card-container">
                 {missingReports.map(missingReport => <MissingReportCard missingReport={missingReport} removeCard={removeCard(missingReport.id)} key={missingReport.id}/>)}
             </div>
